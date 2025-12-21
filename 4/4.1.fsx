b57e70c69b580b749a898d1818ba28615a70c451
Manuel Gutekunst <maanu1@gmail.com>
2025-12-21 11:55:44 +0100

    day 4 - part 2
    Add recursive movement and marking for paper rolls
    
    Extract marking and moving logic into markMoveablePaperrolls,
    movePaperrolls and moveRecursively. Reorganize input/grid setup and
    compute total movable paper rolls by simulating recursive moves.

--- 4/4.1.fsx
+++ 4/4.1.fsx
@@ -4,16 +4,6 @@ let PaperRoll = '@'
 let EmptyPlace = '.'
 let CanBeMoved = 'X'
 
-let inputFile = "input.txt"
-// let inputFile = "demoInput.txt"
-let inputLines = File.ReadAllLines inputFile
-
-let grid =
-    Array2D.init inputLines.Length inputLines.[0].Length (fun i j -> inputLines.[i].[j])
-
-let betterGrid =
-    Array2D.create (inputLines.Length + 2) (inputLines[0].Length + 2) '.'
-
 let countInArray (lookAt: char[]) (grid: char[,]) =
     let mutable count = 0
 
@@ -28,18 +18,53 @@ let countPaperRolls = countInArray [| PaperRoll; CanBeMoved |]
 
 let countMoveablePaperRolls = countInArray [| CanBeMoved |]
 
+let markMoveablePaperrolls (grid: char[,]) =
+    for i in 1 .. (Array2D.length1 grid - 2) do
+        for j in 1 .. (Array2D.length2 grid - 2) do
+            if grid[i, j] = PaperRoll then
+                let subGrid = Array2D.create 3 3 '.'
+                // copy the 3x3 grid around (i,j)
+                Array2D.blit grid (i - 1) (j - 1) subGrid 0 0 3 3
+
+                if countPaperRolls subGrid - 1 < 4 then
+                    Array2D.set grid i j CanBeMoved
+
+let movePaperrolls (grid: char[,]) =
+    grid |> Array2D.map (fun v -> if v = CanBeMoved then EmptyPlace else v)
+
+
+let moveRecursively (grid: char[,]) =
+    let rec loop (grid: char[,]) acc =
+        markMoveablePaperrolls grid
+        let accessiblePaperrolls = grid |> countMoveablePaperRolls
+
+        // printfn "Markings:\n%A\nAccessible rolls: %i" grid accessiblePaperrolls
+
+        if accessiblePaperrolls = 0 then
+            acc |> List.rev
+        else
+            let g = grid |> movePaperrolls
+            loop g (accessiblePaperrolls :: acc)
+
+    loop grid []
+
+
+let inputFile = "input.txt"
+// let inputFile = "demoInput.txt"
+let inputLines = File.ReadAllLines inputFile
+
+let grid =
+    Array2D.init inputLines.Length inputLines.[0].Length (fun i j -> inputLines.[i].[j])
+
+let betterGrid =
+    Array2D.create (inputLines.Length + 2) (inputLines[0].Length + 2) '.'
+
 Array2D.blit grid 0 0 betterGrid 1 1 (Array2D.length1 grid) (Array2D.length2 grid)
 
-for i in 1 .. (Array2D.length1 betterGrid - 2) do
-    for j in 1 .. (Array2D.length2 betterGrid - 2) do
-        if betterGrid[i, j] = PaperRoll then
-            let subGrid = Array2D.create 3 3 '.'
-            // copy the 3x3 grid around (i,j)
-            Array2D.blit betterGrid (i - 1) (j - 1) subGrid 0 0 3 3
+// betterGrid |> markMoveablePaperrolls
+// printfn "Calculated Movings: \n%A" betterGrid
+// betterGrid |> countMoveablePaperRolls |> printfn "%i can be moved"
 
-            if countPaperRolls subGrid - 1 < 4 then
-                Array2D.set betterGrid i j CanBeMoved
-
-
-printfn "Calculated Movings: \n%A" betterGrid
-betterGrid |> countMoveablePaperRolls |> printfn "%i can be moved"
+moveRecursively betterGrid
+|> List.sum
+|> printfn "%i paperrolls can be moved in total"
