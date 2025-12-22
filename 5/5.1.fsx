a3ded60ad5bd0c80e83e3c61eecc161449c65e2b
Manuel Gutekunst <maanu1@gmail.com>
2025-12-22 15:14:04 +0100

    Day 5 - part 2

--- 5/5.1.fsx
+++ 5/5.1.fsx
@@ -10,6 +10,27 @@ let toRange (line: string) =
     | [| f; t |] -> { From = int64 f; To = int64 t }
     | _ -> failwithf "Invalid range format: %s" line
 
+let merge (a: FreshIdRange) (b: FreshIdRange) =
+    match a, b with
+    | { FreshIdRange.From = aFrom
+        FreshIdRange.To = aTo },
+      { FreshIdRange.From = bFrom
+        FreshIdRange.To = bTo } when aFrom <= bFrom && bFrom <= aTo -> Some { From = aFrom; To = max aTo bTo }
+    | _ -> None
+
+let mergeRangesInSeq (ranges: seq<FreshIdRange>) =
+    let folder (current, acc) next =
+        match merge current next with
+        | Some merged -> merged, acc
+        | None -> next, current :: acc
+
+    let first = Seq.head ranges
+    let rest = Seq.tail ranges
+
+    let last, acc = Seq.fold folder (first, []) rest
+
+    List.rev (last :: acc)
+
 let isInRange (ingredient: IngredientId) (range: FreshIdRange) =
     range.From <= ingredient && ingredient <= range.To
 
@@ -33,4 +54,11 @@ let freshIngredients =
     |> Seq.map (isFreshIngredient ranges)
     |> Seq.sumBy (fun v -> if v then 1 else 0)
 
-printf "%A" freshIngredients
+printfn "Number of fresh ingredients %A" freshIngredients
+
+ranges
+|> Seq.sortBy (fun r -> r.From)
+|> mergeRangesInSeq
+|> List.map (fun v -> v.To - v.From + int64 1)
+|> List.sum
+|> printfn "total is: %i"
