86874536462bdc8eb0933a4120b73eb0674989db
Manuel Gutekunst <maanu1@gmail.com>
2025-12-24 14:44:50 +0100

    day 6 part 2

--- 6/6.1.fsx
+++ 6/6.1.fsx
@@ -1,31 +1,74 @@
 open System
 open System.IO
 
-
-let inputFile = "input.txt"
-// let inputFile = "demoInput.txt"
-let inputLines = File.ReadAllLines inputFile
-
 let add (a: int64) (b: int64) = a + b
 let multiply a b = a * b
 
-let getAggregationOperation (op: string) =
-    match op with
-    | "+" -> add, 0
-    | "*" -> multiply, 1
-    | _ -> failwithf "unknown operator %s" op
+let getAggregationOperation =
+    function
+    | "+" -> add
+    | "*" -> multiply
+    | op -> failwithf "unknown operator %s" op
 
-let aggregateLine (l: string[]) =
-    l
-    |> Array.rev
-    |> fun v -> getAggregationOperation (v |> Array.head), Array.tail v
-    |> fun ((operation, neutralElement), list) -> list |> Array.map int64 |> Array.fold operation neutralElement
+let aggregateLine =
+    Array.rev
+    >> fun v -> getAggregationOperation (v |> Array.head), Array.tail v
+    >> fun (operation, list) -> list |> Array.map int64 |> Array.reduce operation
 
 let aggregate (g: string[,]) =
     seq {
         for i in [ 0 .. Array2D.length1 g - 1 ] do
             yield g[i, *] |> aggregateLine
     }
+
+
+let toNumberString (l: char[]) =
+    if Array.forall ((=) ' ') l then None else Some(String l)
+
+let toNumberStrings (g: char[,]) =
+    [ for i in Array2D.length2 g - 1 .. -1 .. 0 -> g[*, i] |> toNumberString ]
+
+type Calculation =
+    { Values: int64 list
+      Operation: int64 -> int64 -> int64 }
+
+let toCalculationGroups (input: seq<option<string>>) =
+    let folder (groups, current) item =
+        match item with
+        | Some v -> groups, v :: current
+        | None ->
+            if current = [] then
+                groups, []
+            else
+                List.rev current :: groups, []
+
+    let groups, current = Seq.fold folder ([], []) input
+
+    if current = [] then groups else List.rev current :: groups
+    |> List.rev
+
+let toCalculation (numbers: string list) =
+    let toNumberAndOperationSymbol (l: string) =
+        (l[.. l.Length - 2], l[l.Length - 1])
+        |> fun (numberString, op) -> numberString |> int64, op
+
+    numbers
+    |> List.map toNumberAndOperationSymbol
+    |> List.fold
+        (fun agg (number, operator) ->
+            { Values = number :: agg.Values
+              Operation =
+                match operator with
+                | ' ' -> agg.Operation
+                | '+' -> add
+                | '*' -> multiply
+                | _ -> failwithf "Unknown operator %c" operator })
+        { Values = List.empty; Operation = add }
+    |> fun c -> { c with Values = List.rev c.Values }
+
+let inputFile = "input.txt"
+// let inputFile = "demoInput.txt"
+let inputLines = File.ReadAllLines inputFile
 
 let l =
     inputLines
@@ -34,3 +77,13 @@ let l =
 let grid = Array2D.init l[0].Length l.Length (fun i j -> l[j][i])
 
 grid |> aggregate |> Seq.sum |> printfn "Sum of calculations: %A"
+
+inputLines
+|> Array.map (fun v -> v.ToCharArray())
+|> fun l -> Array2D.init l.Length l[0].Length (fun i j -> l[i][j])
+|> toNumberStrings
+|> toCalculationGroups
+|> List.map toCalculation
+|> List.map (fun c -> c.Values |> List.reduce (fun u v -> c.Operation u v))
+|> List.sum
+|> printfn "Sum when reading like an cephalopod: %A"
